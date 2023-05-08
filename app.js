0ebd1b761ec6aba761407392baa3a3ffeeb9ac16
vimuths123 <vimuths@yahoo.com>
2023-05-08 21:35:10 +0530

    New changes

--- app.js
+++ app.js
@@ -2,18 +2,15 @@ const button = document.getElementById('testbtn');
 button.addEventListener('click', function () {
     alert('dsds')
     // Make fetch request
-    // fetch('/my-endpoint', {
-    //     method: 'POST',
-    //     body: JSON.stringify({ /* data to send in request body */ }),
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     }
-    // })
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         // Handle response data
-    //     })
-    //     .catch(error => {
-    //         // Handle fetch error
-    //     });
+    fetch('/.netlify/functions/submit-form', {
+        method: 'POST',
+        body: JSON.stringify({ /* data to send in request body */ }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(data => {
+        // Handle response data
+    }).catch(error => {
+        console.error(error)
+    });
 });
