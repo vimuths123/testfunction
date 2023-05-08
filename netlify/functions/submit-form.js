6e835b487cdeeb8df443323d38afd603c52fcc5d
vimuths123 <vimuths@yahoo.com>
2023-05-08 20:15:45 +0530

    New changes

--- netlify/functions/submit-form.js
+++ netlify/functions/submit-form.js
@@ -1,11 +1,17 @@
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
+    // Get the URL of the previous page
+    const referer = event.headers.referer;
 
-    console.log('here')
-    // Do something with the form data
-    // ...
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Form submitted successfully!" }),
+    console.log(referer)
+  
+    // Set the Location header to redirect the user back to the previous page
+    const response = {
+      statusCode: 302,
+      headers: {
+        Location: referer || '/'
+      }
     };
+  
+    return response;
   };
   
